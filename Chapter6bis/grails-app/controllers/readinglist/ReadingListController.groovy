87f3f4886ab20a43e1c3de55562fb2bc53498ddd
Kurt Jennen <kurt.jennen@skynet.be>
2020-09-03 15:45:16 +0200

    Chapter6bis: Not yet working

---
+++ Chapter6bis/grails-app/controllers/readinglist/ReadingListController.groovy
@@ -0,0 +1,17 @@
+package readinglist
+
+import grails.gorm.transactions.Transactional
+
+class ReadingListController {
+
+    def index() { 
+		respond Book.list(params), model: [book: new Book()]
+	}
+	
+	@Transactional
+	def save(Book book) {
+		book.reader = 'Craig'
+		book.save flush:true
+		redirect(action: "index")
+	}
+}
---
+++ Chapter6bis/grails-app/init/readinglist/BootStrap.groovy
@@ -0,0 +1,15 @@
+package readinglist
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+	
+	String reader
+	String isbn
+	String title
+	String author
+	String description
+}
