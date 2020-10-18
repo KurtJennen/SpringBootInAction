9f9426baa4bc565cd059e879c3b5020ba5e90ad7
Kurt Jennen <kurt.jennen@skynet.be>
2020-10-18 19:56:13 +0200

    Chapter8: Not yet working

---
+++ Chapter8/src/test/java/be/ss/readinglist/ReadingListApplicationTests.java
@@ -0,0 +1,16 @@
+package be.ss.readinglist;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class ReadingListApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}
